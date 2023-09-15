4f2d0dd27a46bec2b7e4a1f8fd6b27028a938ab7
ahmza48 <ahmza48.dev@gmail.com>
2023-09-15 15:15:18 +0500

    sorted the result based on prices in ascending order, so cheapest item can be found easily

--- best-gpu-prices-finder/script.py
+++ best-gpu-prices-finder/script.py
@@ -46,6 +46,20 @@ for page in range(1,total_pages+1):
         price_div=item.find('div',class_='item-action')
         price_tag=price_div.find(class_='price-current')
         price=price_tag.strong.text
-        items_found[item_name]={'price':price,'link':link}
+        items_found[item_name]={'price':int(price.replace(',','')),'link':link}
 
-print(items_found)
+# print(items_found)
+
+# items_found.items() it will create all the tuples that'll have key and value pairs key will be item_name and value will be
+# dictionary that has price and link
+# then we are passing a function telling how we want to sort it, this is an anonymous function that takes one parameter x which
+# will be all of these items, then we say x[1] -> this will give us second item that is dictionary(price + link) and then ['price'] tells sort it by price
+# this will return list of tuples
+
+sorted_items=sorted(items_found.items(), key=lambda x: x[1]['price'])
+# print(sorted_items)
+for item in sorted_items:
+    print(item[0])
+    print(item[1]['price'])
+    print(item[1]['link'])
+    print()
